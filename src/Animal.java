075c8e7151cd7a9248d73f533900de0436584cf6
SiLViU <silviuptc@gmail.com>
2025-10-07 16:59:06 +0300

    mostenire

---
+++ src/Animal.java
@@ -0,0 +1,6 @@
+public class Animal {
+    public void sound()
+    {
+        System.out.println("This animal makes a sound");
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Animal cat = new Cat();
 
+        Animal dog = new Dog();
+
+        cat.sound();
+
+        dog.sound();
     }
 }
